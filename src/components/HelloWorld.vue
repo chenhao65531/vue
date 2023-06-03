e20769c72469ecb155b6f12bf56c6d4fd4299ba4
陈浩 <chenhao65531@163.com>
2023-06-03 15:54:31 +0800

    1

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -10,38 +10,166 @@
             <br>
             <span class="e2">在线支付满99减1</span>
             <van-notice-bar class="gonggao" color="#1989fa" background="#ecf9ff"
-                left-icon="info-o">粥品香坊的成功部分归功于其独特的菜单。餐厅提供了多种口味的粥，包括传统的瘦肉粥、鸡肉粥、海鲜粥等，每一碗都经过精心烹制，汤汁醇厚，米粒饱满。此外，粥品香坊还提供了各种美味的配菜和小吃，如炸香蕉、豆腐花等，丰富了整个餐单，满足了不同食客的需求。</van-notice-bar>
+                left-icon="info-o">粥品香坊的成功部分归功于其独特的菜单。餐厅提供了多种口味的粥，包括传统的瘦肉粥、鸡肉粥、海鲜粥等，
+                每一碗都经过精心烹制，汤汁醇厚，米粒饱满。此外，粥品香坊还提供了各种美味的配菜和小吃，如炸香蕉、豆腐花等，丰富了整个餐
+                单，满足了不同食客的需求。</van-notice-bar>
         </div>
         <div class="bt">
             <van-tabs v-model="activeName">
                 <van-tab title="商品" name="a">
-                    <div></div>
+                    <div class="left">
+                        <van-tree-select height="100vw" :items="items" :main-active-index.sync="active">
+                            <template #content>
+
+                                <div v-if="active === 0" class="rexiaobang">
+                                    <van-cell v-for="item in cartItems" :key="item.id">
+                                        <template #icon>
+                                            <img width="100px" height="80px" src="../img/pidan.jpeg" alt="">
+                                            <!-- <van-image width="100px" height="100px" fit="contain" :src="item.image"/> -->
+                                        </template>
+                                        <template #title>
+                                            <span class="cart-item-title">{{ item.name }}</span>
+                                        </template>
+                                        <template #label>
+                                            <van-stepper v-model="item.quantity" :min="0" theme="round" :max="item.stock" />
+                                        </template>
+                                    </van-cell>
+                                </div>
+
+                                <div v-if="active === 1">
+                                    <van-cell v-for="item1 in cartItems1" :key="item1.id">
+                                        <template #icon>
+                                            <img width="100px" height="80px" src="../img/pidan.jpeg" alt="">
+                                            <!-- <van-image width="100px" height="100px" fit="contain" :src="item.image"/> -->
+                                        </template>
+                                        <template #title>
+                                            <span class="cart-item-title">{{ item1.name }}</span>
+                                        </template>
+                                        <template #label>
+                                            <van-stepper v-model="item1.quantity" :min="0" theme="round" :max="item1.stock" />
+                                        </template>
+                                    </van-cell>
+                                </div>
+
+                                <div v-if="active === 2">
+                                    <van-cell v-for="item2 in cartItems2" :key="item2.id">
+                                        <template #icon>
+                                           <img :src="item2.img" width="100px" height="80px"  />
+                                        </template>
+                                        <template #title>
+                                            <span class="cart-item-title">{{ item2.name }}</span>
+                                        </template>
+                                        <template #label>
+                                            <van-stepper v-model="item2.quantity" :min="0" theme="round" :max="item2.stock" />
+                                        </template>
+                                    </van-cell>
+                                </div>
+
+                                <div v-if="active === 3">
+                                    <van-cell v-for="item3 in cartItems3" :key="item3.id">
+                                        <template #icon>
+                                           <img :src="item3.img" width="100px" height="80px"  />
+                                        </template>
+                                        <template #title>
+                                            <span class="cart-item-title">{{ item3.name }}</span>
+                                        </template>
+                                        <template #label>
+                                            <van-stepper v-model="item3.quantity" :min="0" theme="round" :max="item3.stock" />
+                                        </template>
+                                    </van-cell>
+                                </div>
+
+                                <div v-if="active === 4">
+                                    <van-cell v-for="item4 in cartItems4" :key="item4.id">
+                                        <template #icon>
+                                           <img :src="item4.img" width="100px" height="80px"  />
+                                        </template>
+                                        <template #title>
+                                            <span class="cart-item-title">{{ item4.name }}</span>
+                                        </template>
+                                        <template #label>
+                                            <van-stepper v-model="item4.quantity" :min="0" theme="round" :max="item4.stock" />
+                                        </template>
+                                    </van-cell>
+                                </div>
+
+                                <div v-if="active === 5">
+                                    <van-cell v-for="item5 in cartItems5" :key="item5.id">
+                                        <template #icon>
+                                           <img :src="item5.img" width="100px" height="80px"  />
+                                        </template>
+                                        <template #title>
+                                            <span class="cart-item-title">{{ item5.name }}</span>
+                                        </template>
+                                        <template #label>
+                                            <van-stepper v-model="item5.quantity" :min="0" theme="round" :max="item5.stock" />
+                                        </template>
+                                    </van-cell>
+                                </div>
+
+                                <div v-if="active === 6">
+                                    <van-cell v-for="item6 in cartItems6" :key="item6.id">
+                                        <template #icon>
+                                           <img :src="item6.img" width="100px" height="80px"  />
+                                        </template>
+                                        <template #title>
+                                            <span class="cart-item-title">{{ item6.name }}</span>
+                                        </template>
+                                        <template #label>
+                                            <van-stepper v-model="item6.quantity" :min="0" theme="round" :max="item6.stock" />
+                                        </template>
+                                    </van-cell>
+                                </div>
+
+                                <div v-if="active === 7">
+                                    <van-cell v-for="item7 in cartItems7" :key="item7.id">
+                                        <template #icon>
+                                           <img :src="item7.img" width="100px" height="80px"  />
+                                        </template>
+                                        <template #title>
+                                            <span class="cart-item-title">{{ item7.name }}</span>
+                                        </template>
+                                        <template #label>
+                                            <van-stepper v-model="item7.quantity" :min="0" theme="round" :max="item7.stock" />
+                                        </template>
+                                    </van-cell>
+                                </div>
+
+                                <div v-if="active === 8">
+                                    <van-cell v-for="item8 in cartItems8" :key="item8.id">
+                                        <template #icon>
+                                           <img :src="item8.img" width="100px" height="80px"  />
+                                        </template>
+                                        <template #title>
+                                            <span class="cart-item-title">{{ item8.name }}</span>
+                                        </template>
+                                        <template #label>
+                                            <van-stepper v-model="item8.quantity" :min="0" theme="round" :max="item8.stock" />
+                                        </template>
+                                    </van-cell>
+                                </div>
+                            </template>
+                        </van-tree-select>
+                    </div>
                 </van-tab>
-                <van-tab title="评论" name="b">内容 2</van-tab>
-                <van-tab title="商家" name="c">内容 3</van-tab>
+
+                <van-tab title="评论" name="b"></van-tab>
+
+                <van-tab title="商家" name="c"></van-tab>
             </van-tabs>
         </div>
-        <div class="left">
 
-            <van-tree-select height="55vw" :items="items" :main-active-index.sync="active">
-                <template #content>
-                    <van-image v-if="active === 0" src="https://img01.yzcdn.cn/vant/apple-1.jpg" />
-                    <van-image v-if="active === 1" src="https://img01.yzcdn.cn/vant/apple-2.jpg" />
-                </template>
-            </van-tree-select>
+        <div class="show">
+            <van-overlay :show="show" @click="show = false">
+                <div class="wrapper" @click.stop>
+                    <div class="block" />
+                </div>
+             </van-overlay>
 
-            <!-- <van-sidebar v-model="activeKey">
-                <van-sidebar-item title="热销榜" />
-                <van-sidebar-item title="单人特色套餐" />
-                <van-sidebar-item title="特色粥品" />
-                <van-sidebar-item title="精选热菜" />
-                <van-sidebar-item title="爽口凉菜" />
-                <van-sidebar-item title="半成品" />
-                <van-sidebar-item title="饭类" />
-                <van-sidebar-item title="面类" />
-                <van-sidebar-item title="饮品" />
-            </van-sidebar> -->
+                <!-- <van-submit-bar :price="calculateTotalPrice()" currency="￥" button-text="提交订单" >
+                </van-submit-bar> -->
         </div>
+
 
     </div>
 </template>
@@ -52,13 +180,93 @@
 export default {
     data() {
         return {
+            cartItems: [
+                { id: 1, name: '皮蛋瘦肉粥', image: './img/pidan.jpeg', quantity: 0, stock: 10, price: 10 },
+                { id: 2, name: '扁豆焖面', image: './img/pidan.jpeg', quantity: 0, stock: 10, price: 14 },
+                { id: 3, name: '葱花饼', image: './img/pidan.jpeg', quantity: 0, stock: 10, price: 5 },
+                { id: 4, name: '牛肉馅饼', image: './img/pidan.jpeg', quantity: 0, stock: 10, price: 8 },
+                { id: 5, name: '招牌猪肉锅贴', image: './img/pidan.jpeg', quantity: 0, stock: 10, price: 8 },
+                { id: 6, name: '白粥', image: './img/pidan.jpeg', quantity: 0, stock: 10, price: 5 },
+            ],
+            cartItems1: [
+                { id: 1, name: '虾仁面', image: './img/pidan.jpeg', quantity: 0, stock: 10, price: 20 },
+                { id: 2, name: '牛肉面', image: './img/pidan.jpeg', quantity: 0, stock: 10, price: 18 },
+                { id: 3, name: '牛肉盖浇饭', img: "./img/niurou.jpeg", quantity: 0, stock: 10, price: 18 },
+            ],
+            cartItems2: [
+                { id: 1, name: '白粥', img: "./img/xiaren.jpeg", quantity: 0, stock: 10, price: 3 },
+                { id: 2, name: '皮蛋瘦肉粥', img: "./img/niurou.jpeg", quantity: 0, stock: 10, price: 10 },
+            ],
+            cartItems3: [
+                { id: 1, name: '虾仁面', img: './img/xiaren.jpeg', quantity: 0, stock: 10, price: 20 },
+                { id: 2, name: '牛肉面', img: './img/niurou.jpeg', quantity: 0, stock: 10, price: 18 },
+                { id: 3, name: '葱花饼', image: './img/pidan.jpeg', quantity: 0, stock: 10, price: 5 },
+                { id: 4, name: '牛肉馅饼', image: './img/pidan.jpeg', quantity: 0, stock: 10, price: 8 },
+                { id: 5, name: '招牌猪肉锅贴', image: './img/pidan.jpeg', quantity: 0, stock: 10, price: 8 },
+            ],
+            cartItems4: [
+                { id: 1, name: '五香牛肉', img: './img/xiaren.jpeg', quantity: 0, stock: 10, price: 30 },
+                { id: 2, name: '凉拌黄瓜', img: './img/xiaren.jpeg', quantity: 0, stock: 10, price: 10 },
+            ],
+            cartItems5: [
+                { id: 1, name: '梅菜扣肉', img: './img/xiaren.jpeg', quantity: 0, stock: 10, price: 18 },
+            ],
+            cartItems6: [
+                { id: 1, name: '东北大米', img: './img/xiaren.jpeg', quantity: 0, stock: 10, price: 2 },
+                { id: 2, name: '牛肉盖浇饭', img: './img/xiaren.jpeg', quantity: 0, stock: 10, price: 18 },
+            ],
+            cartItems7: [
+                { id: 1, name: '虾仁面', img: './img/xiaren.jpeg', quantity: 0, stock: 10, price: 20 },
+                { id: 2, name: '牛肉面', img: './img/xiaren.jpeg', quantity: 0, stock: 10, price: 18 },
+                { id: 3, name: '扁豆焖面', image: './img/pidan.jpeg', quantity: 0, stock: 10, price: 14 },
+            ],
+            cartItems8: [
+                { id: 1, name: '矿泉水', img: './img/xiaren.jpeg', quantity: 0, stock: 10, price: 2 },
+                { id: 2, name: '可乐', img: './img/xiaren.jpeg', quantity: 0, stock: 10, price: 3.5 },
+            ],
             activeKey: 0,
             activeName: 'a',
             active: 0,
-            items: [{ text: '分组 1' }, { text: '分组 2' }],
+            items: [{ text: '热销榜' }, { text: '单人特色套餐' }, { text: '特色粥品' }, { text: '精选热菜' },
+            { text: '爽口凉菜' }, { text: '半成品' }, { text: '饭类' }, { text: '面类' }, { text: '饮品' }]
         };
     },
+    methods: {
+        calculateTotalPrice() {
+            let totalPrice = 0;
+            for (const item of this.cartItems) {
+                totalPrice += item.quantity * item.price;
+            }
+            for (const item1 of this.cartItems1) {
+                totalPrice += item1.quantity * item1.price;
+            }
+            for (const item2 of this.cartItems2) {
+                totalPrice += item2.quantity * item2.price;
+            }
+            for (const item3 of this.cartItems3) {
+                totalPrice += item3.quantity * item3.price;
+            }
+            for (const item4 of this.cartItems4) {
+                totalPrice += item4.quantity * item4.price;
+            }
+            for (const item5 of this.cartItems5) {
+                totalPrice += item5.quantity * item5.price;
+            }
+            for (const item6 of this.cartItems6) {
+                totalPrice += item6.quantity * item6.price;
+            }
+            for (const item7 of this.cartItems7) {
+                totalPrice += item7.quantity * item7.price;
+            }
+            for (const item8 of this.cartItems8) {
+                totalPrice += item8.quantity * item8.price;
+            }
+            return totalPrice*100;
+        }
+    }
+
 };
+
 </script>
 
 
@@ -67,7 +275,25 @@ export default {
 .all {
     padding-top: 0%;
 }
+.wrapper {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 100%;
+    z-index: 3;
+}
 
+.block {
+    width: 120px;
+    height: 120px;
+    background-color: #fff;
+    
+    z-index: 3;
+}
+.show{
+    z-index: 3;
+    position: absolute;
+}
 .bg {
     filter: blur(10px);
     padding-top: 0px;
@@ -100,6 +326,12 @@ export default {
     left: 50px;
     margin-top: 0px;
     z-index: 2;
+}
+
+.rexiaobang {
+    width: 100%;
+    height: 100%;
+
 }
 
 .name {
@@ -142,11 +374,11 @@ export default {
 }
 
 .left {
-    margin-top: 150px;
-    float: left;
+    margin-top: 0px;
 }
 
 .title {
     left: -30px;
-}</style>
+}
+</style>
   
